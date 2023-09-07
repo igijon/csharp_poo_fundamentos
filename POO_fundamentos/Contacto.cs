9a8fc48702efcf0deeadff6683609469f8531d55
Inma Gijón <igijoninf@gmail.com>
2023-09-07 12:28:02 +0200

    Constructores y destructores

---
+++ POO_fundamentos/Contacto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace POO_fundamentos
+{
+	public class Contacto 
+	{
+		string nombre, apellidos, movil, fijo;
+		public readonly int AA;
+
+		public Contacto(string nombre, string apellidos, string movil)
+		{
+			this.nombre = nombre;
+			this.apellidos = apellidos;
+			this.movil = movil;
+			AA = 666666666;
+		}
+
+		public Contacto(string nombre, string apellidos, string movil, string fijo) :
+			this(nombre, apellidos, movil)
+		{
+			this.fijo = fijo;
+		}
+
+		~Contacto()
+		{
+			//Liberar recursos	
+		}
+
+     
+
+
+    }
+}
+
--- POO_fundamentos/Program.cs
+++ POO_fundamentos/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using POO_fundamentos;
 
+Contacto pepe = new Contacto("Pepe", "García", "123456789");
+Contacto maria = new Contacto("Maria", "Sánchez", "123456789");
+Console.WriteLine(pepe.AA);
+Console.WriteLine(maria);
